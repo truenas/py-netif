0121ccaf4e966b32427d00ac2fca5550427870aa
Jakub Klama <jakub@ixsystems.com>
2016-05-26 21:19:21 +0200

    Fix comparisons against None and other types.

--- netif.pyx
+++ netif.pyx
@@ -1523,6 +1523,9 @@ class Route(object):
         }
 
     def __eq__(self, other):
+        if not isinstance(other, Route):
+            False
+
         return self.network == other.network and \
             self.netmask == other.netmask and \
             self.gateway == other.gateway
